0a42df0393f4e0823de81dc89cd56ac9bbe36d50
Sutanto Kurniawan <tanto@thekubiko.com>
2015-02-23 14:43:41 +0700

    Set variant based on mobvious' device_type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,28 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_variant
+
+  def index
+    # for clarity, but these lines aren't needed:
+    respond_to do |format|
+      format.html
+      # format.html.phone
+      format.html.mobile
+      format.html.tablet
+    end
+  end
+
+  private
+
+    def device_type
+      request.env['mobvious.device_type']
+    end
+    helper_method :device_type
+
+    def set_variant
+      # we can do something like this if we want to use :phone variant (instead of :mobile)
+      # request.variant = { mobile: :phone, tablet: :tablet }[device_type]
+      request.variant = device_type
+    end
 end
